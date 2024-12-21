fe5380500571f5f8b46bab27f1dd6490c1560c13
testervippro <92687973+testervippro@users.noreply.github.com>
2024-12-21 20:40:27 +0700

    Delete selenium-java-automation directory

--- selenium-java-automation/src/main/java/com/thoaikx/exceptions/HeadlessNotSupportedException.java
+++
@@ -1,9 +0,0 @@
-
-package com.thoaikx.exceptions;
-
-public class HeadlessNotSupportedException extends IllegalStateException {
-
-    public HeadlessNotSupportedException(String browser) {
-        super(String.format("Headless not supported for %s browser", browser));
-    }
-}
