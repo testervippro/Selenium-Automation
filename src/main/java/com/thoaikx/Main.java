bef2640962512a2fc05a5cd01554d8d4b1d54299
cuxuanthoai <cuxuanthoai@gmail.com>
2024-12-22 16:40:53 +0700

    add handle record video -read excel , csv

--- src/main/java/com/thoaikx/Main.java
+++ src/main/java/com/thoaikx/Main.java
@@ -2,12 +2,15 @@ package com.thoaikx;
 
 import static java.util.Arrays.sort;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.Collator;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -17,16 +20,28 @@ import net.bytebuddy.implementation.bytecode.ByteCodeAppender.Size;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        System.out.println(System.getProperty("user.home"));
-        Map<String, String> environment = processBuilder.environment();
-        environment.forEach((key, value) -> System.out.println(key +""+ value));
-        // Define the PowerShell command
-        String cmd = "Get-ExecutionPolicy";
+        // Create ObjectMapper instance
+        ObjectMapper objectMapper = new ObjectMapper();
 
+        System.out.println(System.getProperty("${user.home"));
+
+        try {
+            // Read the JSON file and map it to a List of Person records
+            //convert json  to record https://www.danvega.dev/tools/json-to-java-record
+            List<Person> persons = objectMapper.readValue(
+                new File("src/test/resources/data/person.json"),
+                objectMapper.getTypeFactory().constructCollectionType(List.class, Person.class));
+
+            // Print the list of persons
+            persons.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
 }
+record Person(String name, int age, String city) {}
+
 }
 
 
