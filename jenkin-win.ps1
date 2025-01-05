7f87e55f5be915ed2858141f5afcc2171523744e
testervippro <92687973+testervippro@users.noreply.github.com>
2025-01-05 18:29:59 +0700

    Update jenkin-win.ps1

--- jenkin-win.ps1
+++ jenkin-win.ps1
@@ -3,32 +3,26 @@ $JenkinsURL = "https://get.jenkins.io/war-stable/latest/jenkins.war"
 $DownloadPath = Join-Path -Path ([Environment]::GetFolderPath("UserProfile")) -ChildPath "Downloads\jenkins.war"
 
 # Check if the Jenkins WAR file already exists
-if (-not (Test-Path $DownloadPath)) {
-    # Download Jenkins WAR file
-    Invoke-WebRequest -Uri $JenkinsURL -OutFile $DownloadPath -UseBasicParsing
+if (-not (Test-Path -Path $DownloadPath)) {
+    try {
+        # Download Jenkins WAR file
+        Write-Host "Downloading Jenkins WAR file..."
+        Invoke-WebRequest -Uri $JenkinsURL -OutFile $DownloadPath -UseBasicParsing -ErrorAction Stop
+        Write-Host "Download completed successfully."
+    } catch {
+        Write-Error "Failed to download Jenkins WAR file: $_"
+        exit 1
+    }
+} else {
+    Write-Host "Jenkins WAR file already exists. Skipping download."
 }
 
 # Start Jenkins
-Start-Process "java" -ArgumentList "-jar `"$DownloadPath`"
-
-# Wait for Jenkins to start and open the setup page
-$JenkinsReady = $false
-$MaxWaitTime = 300  # Maximum wait time in seconds (5 minutes)
-$StartTime = (Get-Date).Ticks
-
-Do {
-    Start-Sleep -Seconds 10  # Check every 10 seconds
-    $JenkinsReady = (Test-NetConnection -ComputerName localhost -Port 8080 -ErrorAction SilentlyContinue)
-
-    $CurrentTime = (Get-Date).Ticks
-    $ElapsedTime = [math]::Floor(($CurrentTime - $StartTime) / [timespan]::FromSeconds(1).Ticks)
-} until ($JenkinsReady -or $ElapsedTime -ge $MaxWaitTime)
-
-if ($JenkinsReady) {
-    Start-Process -FilePath "http://localhost:8080"
-} else {
-    Write-Host "Jenkins did not start within the expected time frame."
+try {
+    Write-Host "Starting Jenkins..."
+    Start-Process -FilePath "java" -ArgumentList "-jar `"$DownloadPath`"" -NoNewWindow
+    Write-Host "Jenkins started successfully."
+} catch {
+    Write-Error "Failed to start Jenkins: $_"
+    exit 1
 }
-
-# Delete WAR file if necessary
-# rmdir /s /q "%USERPROFILE%\.jenkins"
