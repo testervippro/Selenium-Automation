5d7bcbb48efe77c980aed192e7a1961e6be91662
cuxuanthoai <cuxuanthoai@gmail.com>
2025-01-29 22:27:20 +0700

    back to allure generate

--- src/main/java/com/thoaikx/report/AllureManager.java
+++ src/main/java/com/thoaikx/report/AllureManager.java
@@ -44,7 +44,7 @@ public class AllureManager {
         //current directory
         Path targetDirectory = Path.of(System.getProperty("user.dir") ,"target");
 
-        String cmd = "cd " + targetDirectory + " && allure server";
+        String cmd = "cd " + targetDirectory + " && allure generate";
         ProcessManager.executeCommand(cmd);
 
 
