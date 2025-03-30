1ee738fb31123e8b6c0d8c6a8ae85d8ebd77434c
testervippro <92687973+testervippro@users.noreply.github.com>
2025-03-30 13:07:44 +0700

    Update RunSeleniumGridAndRecordVideo.ps1

--- RunSeleniumGridAndRecordVideo.ps1
+++ RunSeleniumGridAndRecordVideo.ps1
@@ -1,20 +1,26 @@
-# Before run this cmd pls install podman
-# Create a Docker network
-docker network create grid
+$podmanPath = Join-Path -Path $env:USERPROFILE -ChildPath ".m2\repository\mylib\bin\podman.exe"
 
-# Run Selenium Standalone Chrome container
-docker run -d -p 4444:4444 -p 6900:5900 --net grid --name selenium --shm-size="2g" selenium/standalone-chrome:4.30.0-20250323
+Write-Host "Podman detected: $($podmanPath)" -ForegroundColor Green
+Write-Host "Starting Podman machine..."
+& $podmanPath machine start
 
-# Run video recording container
-docker run -d --net grid --name video -v ./videos:/videos selenium/video:ffmpeg-7.1.1.1-20250323
+Write-Host "Creating Podman network 'grid'..."
+& $podmanPath network create grid
 
-# Run your tests...
+Write-Host "Starting Selenium Standalone Chrome..."
+& $podmanPath run -d -p 4444:4444 -p 6900:5900 --net grid --name selenium --shm-size="2g" selenium/standalone-chrome:4.30.0-20250323
 
+Write-Host "🎥 Starting video recording container..."
+& $podmanPath run -d --net grid --name video -v ${PWD}/videos:/videos selenium/video:ffmpeg-7.1.1.1-20250323
+
+Write-Host " Running Maven tests..."
 mvn test -Dsuite=local
 
-# Cleanup: Stop and remove containers and network
-docker stop video ; docker rm video
-docker stop selenium ; docker rm selenium
-docker network rm grid
+Write-Host "Stopping and removing containers..."
+& $podmanPath stop video
+& $podmanPath rm video
+& $podmanPath stop selenium
+& $podmanPath rm selenium
+& $podmanPath network rm grid
 
-# run this cmd powershell -ep Bypass -f RunSeleniumGridAndRecordVideo.ps1
+Write-Host "Cleanup complete!"
